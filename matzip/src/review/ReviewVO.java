49f13545a1c3110d971d5d78c8f53a1cc9624bef
shw89 <shw89@DESKTOP-J0GA736>
2019-01-02 15:11:58 +0900

    리뷰 VO, SQL  작업

--- matzip/src/review/ReviewVO.java
+++ matzip/src/review/ReviewVO.java
@@ -8,7 +8,64 @@ public class ReviewVO {
 	private String ID;
 	private String subject;
 	private String images;
+	private String context;
 	private int sympathy;
 	private Date reg_date;
 	private float starPoint;
+	public int getRestaurantNo() {
+		return restaurantNo;
+	}
+	public void setRestaurantNo(int restaurantNo) {
+		this.restaurantNo = restaurantNo;
+	}
+	public int getReviewNo() {
+		return reviewNo;
+	}
+	public void setReviewNo(int reviewNo) {
+		this.reviewNo = reviewNo;
+	}
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getImages() {
+		return images;
+	}
+	public void setImages(String images) {
+		this.images = images;
+	}
+	public String getContext() {
+		return context;
+	}
+	public void setContext(String context) {
+		this.context = context;
+	}
+	public int getSympathy() {
+		return sympathy;
+	}
+	public void setSympathy(int sympathy) {
+		this.sympathy = sympathy;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public float getStarPoint() {
+		return starPoint;
+	}
+	public void setStarPoint(float starPoint) {
+		this.starPoint = starPoint;
+	}
+	
+	
 }
