fac28e47eaac74dde25bf145c4d99d5224f5eafb
user <user@DESKTOP-VSR60N5>
2019-01-07 09:32:22 +0900

    블랙리스트 목록보기/등록 최종

--- matzip/WebContent/common/BlackList/blackListView.jsp
+++ matzip/WebContent/common/BlackList/blackListView.jsp
@@ -1,39 +1,69 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page session="true" contentType="text/html; charset=utf-8" %>
 <%@ taglib prefix="s" uri="/struts-tags" %>
-<!DOCTYPE html>
-<html>
+
+<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta charset="UTF-8">
-<title>블랙리스트</title>
+	<title>BlackList</title>
 </head>
-<body>
-<table class="table table-striped" width="600" border="0" cellpadding="2">
-<tr>
-<td align="center"><h2>블랙리스트</h2></td>
-</tr>
-<tr>
-  <td height="20"></td>
-</tr>
-</table>
-<table class="table table-striped" width="600" border="0" cellspacing="0" cellpadding="0">
-           <tr align="center" bgcolor="#F3F3F3">
-      		<td width="50"><strong>번호</strong></td></tr>
-      		
- <s:iterator value="list">
- <s:url id="viewURL" action="BlackListViewAction">
- <s:param name="phoneNumber">
+  
+  <body>
+  	<table width="600" border="0" cellspacing="0" cellpadding="2">
+  		<tr>
+  			<td align="center"><h2>BlackList</h2></td>
+  		</tr>
+  		<tr>
+  			<td height="20"></td>
+  		</tr>
+  	</table>
+  	
+  <div style="text-align:center">
+	<table width="600" border="0" cellspacing="0" cellpadding="2">
+	      <tr align="center" bgcolor="#F3F3F3">
+      		<td width="50"><strong>번호</strong></td>
+	
+      	      <tr bgcolor="#777777">
+        		<td height="1" colspan="5"></td>
+      	      </tr>
+
+	      <s:iterator value="list" status="stat">
+
+		<s:url id="viewURL" action="blackList.BlackListViewAction" >
+			<s:param name="phoneNumber">
 				<s:property value="phoneNumber" />
 			</s:param>
- </s:url>
-</s:iterator>
-   <s:if test="list.size() <= 0">
-    <tr bgcolor="#FFFFFF"  align="center">
-    <td colspan="5">등록된 번호가 없습니다.</td></tr>						
+			<s:param name="currentPage">
+				<s:property value="currentPage" />
+			</s:param>
+		</s:url>
+			
+     	      <tr bgcolor="#FFFFFF"  align="center">
+        		<td><s:property value="phoneNumber" /></td>
+                  <td align = "center"><%-- <s:property value = "phoneNumber" /> --%></td>
+      	      </tr>
+	      </s:iterator>
+			
+	      <s:if test="list.size() <= 0">
+				
+	      <tr bgcolor="#FFFFFF"  align="center">
+		<td colspan="5">등록된 게시물이 없습니다.</td>
+                  </tr>						
 	      <tr bgcolor="#777777">
       		<td height="1" colspan="5"></td>
     	      </tr>
+    		
 	      </s:if>
-     
-</body>
+			
+	      <tr align="center">
+    		<td colspan="5"><s:property value="pagingHtml"  escape="false" /></td>
+    	      </tr>
+    	
+    	      <tr align="right">
+    		<td colspan="5">
+		</td>
+    	       </tr>
+	</div>
+	</table>
+   </body>
 </html>
--- matzip/src/blackList/BlackListViewAction.java
+++ matzip/src/blackList/BlackListViewAction.java
@@ -4,23 +4,36 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
 
 import com.ibatis.common.resources.Resources;
 import com.ibatis.sqlmap.client.SqlMapClient;
 import com.ibatis.sqlmap.client.SqlMapClientBuilder;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class BlackListViewAction extends ActionSupport{
+import blackList.BlackListPagingAction;
+import member.MemberVO;
+
+public class BlackListViewAction extends ActionSupport  implements SessionAware{
 	
 	public static Reader reader;
 	public static SqlMapClient sqlMapper;
 	
 	public List<BlackListVO>list = new ArrayList<BlackListVO>();
 	
-	private BlackListVO paramClass = new BlackListVO();
-	private BlackListVO resultClass = new BlackListVO();
+	private int currentPage = 1;	//현재 페이지
+	private int totalCount; 		// 총 게시물의 수
+	private int blockCount = 10;	// 한 페이지의  게시물의 수
+	private int blockPage = 5; 	// 한 화면에 보여줄 페이지 수
+	private String pagingHtml; 	//페이징을 구현한 HTML
+	private BlackListPagingAction page; 	// 페이징 클래스
+
+	private MemberVO memberResultClass;
+    private Map session;
+    
 	
-	private int phoneNumber;
 	
 	public BlackListViewAction() throws IOException{
 		reader = Resources.getResourceAsReader("sqlMapConfig.xml");
@@ -30,9 +43,78 @@ public class BlackListViewAction extends ActionSupport{
 	
 	public String execute() throws Exception{
 		
-		list = sqlMapper.queryForList("selectAll");
+		list = sqlMapper.queryForList("blackList.selectAll");
+		totalCount=list.size();//전체 글 개수
+		page=new BlackListPagingAction(currentPage, totalCount, blockCount, blockPage);
+		pagingHtml=page.getPagingHtml().toString();//페이지HTML생성.
+		
+		int lastCount = totalCount;
+		
+		if(page.getEndCount()<totalCount)
+			lastCount=page.getEndCount()+1;
+		
+		list=list.subList(page.getStartCount(), lastCount);
+		
+		memberResultClass = (MemberVO) sqlMapper.queryForObject("member.selectOne", session.get("ID"));
 		
 		return SUCCESS;
+	}
+	
+
+	public List<BlackListVO> getList() {
+		return list;
+	}
+
+	public void setList(List<BlackListVO> list) {
+		this.list = list;
+	}
+
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+
+	public int getTotalCount() {
+		return totalCount;
+	}
+
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
+
+	public int getBlockCount() {
+		return blockCount;
+	}
+
+	public void setBlockCount(int blockCount) {
+		this.blockCount = blockCount;
+	}
+
+	public int getBlockPage() {
+		return blockPage;
+	}
+
+	public void setBlockPage(int blockPage) {
+		this.blockPage = blockPage;
+	}
+
+	public String getPagingHtml() {
+		return pagingHtml;
+	}
+
+	public void setPagingHtml(String pagingHtml) {
+		this.pagingHtml = pagingHtml;
+	}
+
+	public BlackListPagingAction getPage() {
+		return page;
+	}
+
+	public void setPage(BlackListPagingAction page) {
+		this.page = page;
 	}
 
 	public static Reader getReader() {
@@ -47,33 +129,23 @@ public class BlackListViewAction extends ActionSupport{
 		return sqlMapper;
 	}
 
+	public MemberVO getMemberResultClass() {
+		return memberResultClass;
+	}
+
+	public void setMemberResultClass(MemberVO memberResultClass) {
+		this.memberResultClass = memberResultClass;
+	}
+
+	public Map getSession() {
+		return session;
+	}
+
+	public void setSession(Map session) {
+		this.session = session;
+	}
+
 	public static void setSqlMapper(SqlMapClient sqlMapper) {
 		BlackListViewAction.sqlMapper = sqlMapper;
 	}
-
-	public BlackListVO getParamClass() {
-		return paramClass;
-	}
-
-	public void setParamClass(BlackListVO paramClass) {
-		this.paramClass = paramClass;
-	}
-
-	public BlackListVO getResultClass() {
-		return resultClass;
-	}
-
-	public void setResultClass(BlackListVO resultClass) {
-		this.resultClass = resultClass;
-	}
-
-	public int getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(int phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	
-
 }
