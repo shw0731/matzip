183594919a2410fd4d54ee489473ec52bf425067
shw89 <shw89@DESKTOP-J0GA736>
2019-01-08 12:44:30 +0900

    Main top 리스트

--- matzip/src/member/MainAction.java
+++ matzip/src/member/MainAction.java
@@ -2,6 +2,8 @@ package member;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
@@ -13,10 +15,16 @@ import com.ibatis.common.resources.Resources;
 import com.ibatis.sqlmap.client.SqlMapClient;
 import com.ibatis.sqlmap.client.SqlMapClientBuilder;
 import com.opensymphony.xwork2.ActionSupport;
+
+
+import restaurantPage.BoardVO;
 public class MainAction extends ActionSupport implements SessionAware {
 	
 	public static Reader reader;
 	public static SqlMapClient sqlMapper;
+	
+
+	private List<BoardVO> restList = new ArrayList<BoardVO>();
 	private Map session;
 	private MemberVO resultClass = new MemberVO();
 	public MainAction() throws IOException {
@@ -25,12 +33,25 @@ public class MainAction extends ActionSupport implements SessionAware {
 		reader.close();
 	}
 	public String execute() throws Exception{
+		restList = sqlMapper.queryForList("rest.selectAll"); 
 		if(session.get("ID")!=null)
 		resultClass = (MemberVO) sqlMapper.queryForObject("member.selectOne", session.get("ID"));	
 				return SUCCESS;
 			
 		}
 		
+	public List<BoardVO> getRestList() {
+		return restList;
+	}
+	public void setRestList(List<BoardVO> restList) {
+		this.restList = restList;
+	}
+	public MemberVO getResultClass() {
+		return resultClass;
+	}
+	public void setResultClass(MemberVO resultClass) {
+		this.resultClass = resultClass;
+	}
 	public Map getSession() {
 		return session;
 	}
